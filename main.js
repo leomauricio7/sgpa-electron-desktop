fb9f165a0c38ea0a9424beba100ddabcfa03ca8a
Leonardo Mauriico <leomauricio7@gmail.com>
2020-01-27 10:57:43 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,75 @@
+const { app, BrowserWindow } = require('electron')
+// Mantém a referência global do objeto da janela.
+// se você não fizer isso,
+// a janela será fechada automaticamente
+// quando o objeto JavaScript for coletado como lixo.
+let win
+
+function createWindow () {
+  // Criar uma janela de navegação.
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    fullscreen: false,
+    fullscreenable: true,
+    darkTheme: true,
+    center: true,
+    show: false,
+    alwaysOnTop: false,
+    maximizable: true,
+    minimizable: true,
+    resizable: true,
+    opacity: 1.0,
+    autoHideMenuBar: false,
+    icon: __dirname + './public/img/icon.png',
+    acceptFirstMouse: true,
+    title: 'SGPA',
+    webPreferences: {
+      nodeIntegration: true
+    },
+  })
+
+  // and load the index.html of the app.
+  win.loadFile('index.html')
+
+  // Open the DevTools.
+  win.webContents.openDevTools()
+
+  // Emitido quando a janela é fechada.
+  win.on('closed', () => {
+    // Elimina a referência do objeto da janela, geralmente você iria armazenar as janelas
+    // em um array, se seu app suporta várias janelas, este é o momento
+    // quando você deve excluir o elemento correspondente.
+    win = null
+  })
+
+  win.once('ready-to-show', () => {
+    win.show()
+  })
+}
+
+// Este método será chamado quando o Electron tiver finalizado
+// a inicialização e está pronto para criar a janela browser.
+// Algumas APIs podem ser usadas somente depois que este evento ocorre.
+app.on('ready', createWindow)
+
+// Finaliza quando todas as janelas estiverem fechadas.
+app.on('window-all-closed', () => {
+  // No macOS é comum para aplicativos e sua barra de menu 
+  // permaneçam ativo até que o usuário explicitamente encerre com Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
+
+
+// Neste arquivo, você pode incluir o resto do seu aplicativo especifico do processo
+// principal. Você também pode colocar eles em arquivos separados e requeridos-as aqui.
